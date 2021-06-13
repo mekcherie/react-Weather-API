c7292618707c1f44474c2a48d4b0923dd0122005
Meklit Cherie <makywonde347@yahoo.com>
2021-06-12 18:58:12 -0700

    added a new folder RasioButton

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './Weather.css'
 
+import RadioButton from './RadioButton'
+
 function Weather() {
     const [zip, setZip] = useState('95123')
     const [unit, setUnit] = useState('')
@@ -15,11 +17,35 @@ function Weather() {
                     /> 
                 <button>Submit</button>
 
-                <select onChange = {e=> setUnit(e.target.value)}>
+                <select
+                    value={unit}
+                    onChange = {e=> setUnit(e.target.value)}
+                >
                     <option value="metric">Celcius</option>
                     <option value="imperial">Fahrenheit</option>
                     <option value="standard">Kelvin</option>
                 </select>
+
+                <RadioButton
+                label = "metric"
+                name="unit"
+                checked={unit === "metric" }
+                onChange={() =>setUnit('metric')}
+                />
+
+                <RadioButton
+                label = "imperial"
+                name="unit"
+                checked={unit === "imperial" }
+                onChange={() =>setUnit('imperial')}
+                />
+
+                <RadioButton
+                label = "standard"
+                name="unit"
+                checked={unit === "standard" }
+                onChange={() =>setUnit('standard')}
+                />
             </form>
         </div>
     )
